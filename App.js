baad73e1192c3e6cf31d948a48362e1ad241a2c7
Jenni <jentsu.karoliina@hotmail.com>
2021-11-24 20:17:02 +0200

    done app

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import * as Speech from 'expo-speech';
+
 
 export default function App() {
+
+  const [input, setInput] = useState('')
+
+  
+  const speak = () => {
+    Speech.speak(input);
+    setInput('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.inputWrapper}>
+        <TextInput style={styles.input} placeholder='What would you like me to say?' onChangeText={(input) => setInput(input)} value={input}/>
+      </View>
+      <View style={styles.inputWrapper}>
+        <Button title='SPEAK' onPress={speak} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +33,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputWrapper: {
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+
+  },
+  input: {
+    width: 300,
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 1
   },
 });
